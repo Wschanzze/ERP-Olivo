d5ffe01a21d05fe8be84643efaa7be49cf250277
Joshua Gonzalez <josuugonzalezz@gmail.com>
2026-09-23 15:40:16 -0300

    chore: eliminar scripts de debug

--- debug_estado.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
-
-from apps.personal.models import OrdenTrabajo
-
-o = OrdenTrabajo.objects.get(id=2)
-print(f"Orden 2 Estado: {o.estado}")
